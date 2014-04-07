5ee6ca6096042cf40ae956203c14088edce95e2d
yanniks <kontakt@yanniks.de>
2014-04-07 21:29:29 +0200

    initial CyanDream commit
    Change-Id: I8ebc9cb35e3046efce0f1514e39c091479daf8b8

--- cd.mk
+++ cd.mk
@@ -1,16 +1,16 @@
 $(call inherit-product, device/sony/tsubasa/full_tsubasa.mk)
 
 # Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cyandream/config/gsm.mk)
 
 # Enhanced NFC
-#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/cyandream/config/nfc_enhanced.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT25i BUILD_FINGERPRINT=Sony/LT25i_1268-6904/LT25i:4.1.2/9.1.A.1.140/c_t_tw:user/release-keys PRIVATE_BUILD_DESC="LT25i-user 4.1.2 9.1.A.1.140 c_t_tw test-keys"
 
 
-PRODUCT_NAME := cm_tsubasa
+PRODUCT_NAME := cd_tsubasa
 PRODUCT_DEVICE := tsubasa
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_tsubasa-eng
+add_lunch_combo cd_tsubasa-eng
